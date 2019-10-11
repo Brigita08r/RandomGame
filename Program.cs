4773ef5f1fe8c7f406e038541d59db768426328f
Brigita08r <brigita08r@gmail.com>
2019-10-11 18:36:31 +0300

    added file
    add

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RandomGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random RandomClass = new Random();
+            int x = RandomClass.Next(1, 10);
+
+            Console.WriteLine("Guess the number between 1 and 10");
+            int guess = 0;
+
+            while (guess != x)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == x)
+                {
+                    Console.WriteLine("well done!");
+                    
+                }
+                else if (guess != x)
+                {
+                    if (guess < x)
+                    {
+                        Console.WriteLine("Number is higher");
+                        
+                    }
+                    else if (guess > x)
+                    {
+
+                        Console.WriteLine("Nope, its lower");
+                        
+                    }
+                } 
+
+            }
+        }
+    }
+}
